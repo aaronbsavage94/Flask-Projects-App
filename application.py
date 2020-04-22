8da472e84716d85b3c7bc2079c86a62a554f2556
aaronbsavage94 <aaronbsavage94@users.noreply.github.com>
2020-04-22 19:29:12 -0400

    Add random quotes to Home Page

--- application.py
+++ application.py
@@ -18,50 +18,99 @@ def picker():
 def covid():
     return render_template('covid_tracking.html', title="COVID-19 Tracking")
 
+@app.route('/quote', methods=['POST'])
+def getQuote():
+    results = []
+
+    try:
+        url = "https://quotes15.p.rapidapi.com/quotes/random/"
+
+        querystring = {"language_code":"en"}
+
+        headers = {
+            'x-rapidapi-host': "quotes15.p.rapidapi.com",
+            'x-rapidapi-key': "513b4d165fmsh4c349204d03662dp1d7b72jsn7bad13d69a6f"
+            }
+
+        response = requests.request("GET", url, headers=headers, params=querystring, timeout=1)
+        data = response.json()
+
+        
+        quote = str(data['content'] + "\n")
+        originator = data['originator']
+        author = "--" + str(originator['name'])
+        
+        results.append(quote)
+        results.append(author)
+
+        return render_template('home.html', results=results)
+
+    except:
+        results.append("Error encountered, please try again later.")
+        return render_template('home.html', results=results)
+
 @app.route('/trackCOVID', methods=['POST'])
 def getCOVIDData():
+    results = []
 
-    response = requests.get('https://finnhub.io/api/v1/covid19/us?token=bpkgs0vrh5rcgrlra5v0')
-    data = response.json()
+    try:
+        response = requests.get('https://finnhub.io/api/v1/covid19/us?token=bpkgs0vrh5rcgrlra5v0')
+        data = response.json()
 
-    return render_template('covid_tracking.html', title="COVID-19 Tracking", results=data)
+        if "Invalid API" in data:
+            results.append("Error encountered, please try again later.")
+            return render_template('covid_tracking.html', title="COVID-19 Tracking", results=results)
 
+        else:
+            return render_template('covid_tracking.html', title="COVID-19 Tracking", results=data)
+
+    
+    except:
+        results.append("Error encountered, please try again later.")
+        return render_template('covid_tracking.html', title="COVID-19 Tracking", results=results)
 
 @app.route('/requestStock', methods=['POST'])
 def requestStock():
 
     picker = request.form["ticker"]
-
-    url = "https://finnhub-realtime-stock-price.p.rapidapi.com/quote"
-
-    queryString = {"symbol":picker}
-
-    headers = {
-        'x-rapidapi-host': "finnhub-realtime-stock-price.p.rapidapi.com",
-        'x-rapidapi-key': "513b4d165fmsh4c349204d03662dp1d7b72jsn7bad13d69a6f"
-        }
-
-    response = requests.request("GET", url, headers=headers, params=queryString)
-    data = response.json()
-
     results = []
-    ticker = "Ticker: " + picker
-    current = "Current price: " + str(data['c'])
-    high = "High of the day: " + str(data['h'])
-    low = "Low of the day: " + str(data['l'])
-    opening = "Opening price of the day: " + str(data['o'])
-    previous = "Previous close price: " + str(data['pc'])
-    timestamp = "Timestamp: " + str(data['t'])
 
-    results.append(ticker)
-    results.append(current)
-    results.append(high)
-    results.append(low)
-    results.append(opening)
-    results.append(previous)
-    results.append(timestamp)
+    try:
+        url = "https://finnhub-realtime-stock-price.p.rapidapi.com/quote"
 
-    return render_template('stockcheck.html', title="Stock Price Checker", results=results)
+        queryString = {"symbol":picker}
+
+        headers = {
+            'x-rapidapi-host': "finnhub-realtime-stock-price.p.rapidapi.com",
+            'x-rapidapi-key': "513b4d165fmsh4c349204d03662dp1d7b72jsn7bad13d69a6f"
+            }
+
+        response = requests.request("GET", url, headers=headers, params=queryString)
+        data = response.json()
+
+        
+        ticker = "Ticker: " + picker
+        current = "Current price: " + str(data['c'])
+        high = "High of the day: " + str(data['h'])
+        low = "Low of the day: " + str(data['l'])
+        opening = "Opening price of the day: " + str(data['o'])
+        previous = "Previous close price: " + str(data['pc'])
+        timestamp = "Timestamp: " + str(data['t'])
+
+        results.append(ticker)
+        results.append(current)
+        results.append(high)
+        results.append(low)
+        results.append(opening)
+        results.append(previous)
+        results.append(timestamp)
+
+        return render_template('stockcheck.html', title="Stock Price Checker", results=results)
+
+    except:
+        results.append("Error encountered, please try again later.")
+        return render_template('stockcheck.html', title="Stock Price Checker", results=results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
