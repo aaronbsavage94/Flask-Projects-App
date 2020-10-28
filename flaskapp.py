babfa3d332118d988f7426accf9b140ce532a616
aaronbsavage94 <aaronbsavage94@users.noreply.github.com>
2020-10-28 15:42:08 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -574,7 +574,7 @@ def greyImage():
             uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
 
         #Concatenate file path
-        filestring = "./uploads/" + filename
+        filestring = "./instance/uploads/" + filename
         filename = os.path.join(filestring)
         original = io.imread(filename)
 
