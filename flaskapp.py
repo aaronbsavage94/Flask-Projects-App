5f222aa0947f93fdd8c704672a5a378d43e171a0
aaronbsavage94 <aaronbsavage94@users.noreply.github.com>
2020-10-28 16:14:22 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -189,7 +189,7 @@ def searchYouTube():
                 results.append('<div class="col-sm">')
                 results.append("<a href= " + i['link'] + " target=_blank>" + i['title'] + "</a>")
                 results.append("<p>" + description + "</p></div>")
-                results.append("</div><br>")
+                results.append("</div><br><br>")
         
         #Direct output to form
         return render_template('youtube.html', title="Search YouTube", results=results)
